329a1024f207ea942d5f784e73d9cf398b226c19
Noveren <1079148276@qq.com>
2024-01-15 00:40:43 +0800

    先用好 c，多积攒经验

--- STM32F1xx.lua
+++ STM32F1xx.lua
@@ -73,9 +73,9 @@ function __config(target, makefile)
     )
     target:add("ldflags", cpu, ins,
         "-T./STM32F103C8TX_FLASH.ld",
-        -- "-specs=nosys.specs",
+        "-specs=nosys.specs",
         "-specs=nano.specs",
-        "-lc", "-lm", "-lnosys", "-Wl,--gc-sections",
+        "-lc", "-lnosys", "-Wl,--gc-sections",
         { force = true }
     )
     target:add("includedirs",
